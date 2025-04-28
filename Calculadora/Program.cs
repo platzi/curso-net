84042fec100151b31dba1fc876da6aa1f196f7b8
aminespinoza10 <amin.espinoza@outlook.com>
2025-04-28 12:10:57 -0500

    Ejercicio de Calculadora agregado

---
+++ Calculadora/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int Suma(int numero1, int numero2)
+{
+    Console.WriteLine($"La suma de {numero1} + {numero2} es: {numero1 + numero2}");
+    int resultado = numero1 + numero2;
+    return resultado;
+}
+
+int numeroResultado = Suma(5, 10);
+
+Console.WriteLine($"El resultado de la suma es: {numeroResultado}");
+//Suma(5, 10);
