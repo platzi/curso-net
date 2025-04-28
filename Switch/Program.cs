96c7fd27a958fda34b1875de51ed29fc695bd500
aminespinoza10 <amin.espinoza@outlook.com>
2025-04-28 12:01:25 -0500

    Ejercicio Switch agregado

---
+++ Switch/Program.cs
@@ -0,0 +1,24 @@
+﻿List<string> nombres2 = new List<string>();
+nombres2.Add("Oscar");
+nombres2.Add("Felipe"); 
+nombres2.Add("Amin");
+nombres2.Add("Jorge");
+
+switch (nombres2[3])
+{
+    case "Oscar":
+        Console.WriteLine("El nombre es Oscar");
+        break;
+    case "Felipe":
+        Console.WriteLine("El nombre es Felipe");
+        break;
+    case "Amin":
+        Console.WriteLine("El nombre es Amin");
+        break;
+    case "Jorge":
+        Console.WriteLine("El nombre es Jorge");
+        break;
+    default:
+        Console.WriteLine("No se encontró el nombre");
+        break;
+}
