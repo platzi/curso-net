857cdc2e489145ff4d45c6d05c74367c94f68a20
aminespinoza10 <amin.espinoza@outlook.com>
2025-04-28 11:54:33 -0500

    Ejercicio de Arreglos agregado

---
+++ Arreglos/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] nombres= new string[4];
+nombres[0] = "Oscar";
+nombres[1] = "Felipe";
+nombres[2] = "Amin";
+nombres[3] = "Jorge";
+
+Console.WriteLine("Los nombres de mi arreglo son: ");
+foreach(string nombre in nombres)
+{
+    Console.WriteLine(nombre);
+}
+
+List<string> nombres2 = new List<string>();
+nombres2.Add("Oscar");
+nombres2.Add("Felipe"); 
+nombres2.Add("Amin");
+nombres2.Add("Jorge");
+
+Console.WriteLine("Los nombres de mi lista son: ");
+foreach(string nombre in nombres2)
+{
+    Console.WriteLine(nombre);
+}
