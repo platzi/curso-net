a3c67ad8c4eed76a1e4ec4164881cb721eb91322
aminespinoza10 <amin.espinoza@outlook.com>
2025-04-28 11:43:07 -0500

    Ejercicio de Foreach agregado

---
+++ Foreach/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Por favor ingresa tu nombre");
+string nombre = Console.ReadLine();
+
+foreach (char letra in nombre)
+{
+    Console.WriteLine(letra);
+}
+
+int contador = 10;
+foreach (int i in Enumerable.Range(1, contador))
+{
+    Console.WriteLine($"Contador: {contador}");
+    contador--;
+}
