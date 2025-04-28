d5b8428b15facf3030892395f6e9ccf55d13cc2c
aminespinoza10 <amin.espinoza@outlook.com>
2025-04-28 10:11:49 -0500

    Ejercicios actualizados

---
+++ Constantes/Program.cs
@@ -0,0 +1,14 @@
+﻿const string nombre = "Amin";
+
+string apellido ="Espinoza";
+apellido = "Espinoza de los Monteros"; 
+
+const int edad = 40;
+int edad2 = 40;
+
+bool isProfePlatzi = true;
+var precio1 = 14.5;
+double precio = 10.5;
+
+Console.WriteLine("Hola, " + nombre + " " + apellido + " tienes " + edad + " años y el precio es: " + precio1);
+
---
+++ Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿var nombre = "Amin";
+string apellido ="Espinoza";
+
+apellido = "Espinoza de los Monteros"; 
+
+var edad = 40;
+int edad2 = 40;
+
+bool isProfePlatzi = true;
+
+var precio1 = 14.5;
+double precio = 10.5;
+
+Console.WriteLine("Hola, " + nombre + " " + apellido + " tienes " + edad + " años y el precio es: " + precio1);
+
