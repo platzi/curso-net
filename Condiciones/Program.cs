cd8e1b7e677e47628c0399f241400b7d30c8defb
aminespinoza10 <amin.espinoza@outlook.com>
2025-04-28 10:38:40 -0500

    Ejercicio de condiciones agregado

---
+++ Condiciones/Program.cs
@@ -0,0 +1,13 @@
+﻿int edad = 18;
+
+Console.WriteLine("Introduce tu edad: ");
+edad = Convert.ToInt32(Console.ReadLine());
+
+if (edad >= 18) {
+    Console.WriteLine("Eres mayor de edad");
+    Console.WriteLine("Puedes votar");
+}
+else {
+    Console.WriteLine("Eres menor de edad");
+    Console.WriteLine("No puedes votar");
+}
