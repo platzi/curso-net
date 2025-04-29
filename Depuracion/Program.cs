47b5030eb12578f03dda4c0a48ab4f87f92bcf0f
aminespinoza10 <amin.espinoza@outlook.com>
2025-04-29 10:03:29 -0500

    Ejercicio de depuración agregado

---
+++ Depuracion/Program.cs
@@ -0,0 +1,22 @@
+﻿List<string> listaNombres = new List<string>();
+List<string> listaApellidos = new List<string>();
+
+listaNombres.Add("Juan");
+listaNombres.Add("Pedro");
+listaNombres.Add("Maria");
+listaNombres.Add("Ana");
+listaNombres.Add("Luis");
+
+listaApellidos.Add("Gomez");
+listaApellidos.Add("Lopez");
+listaApellidos.Add("Martinez");
+listaApellidos.Add("Hernandez");
+listaApellidos.Add("Garcia");
+
+foreach (string nombre in listaNombres)
+{
+    foreach (string apellido in listaApellidos)
+    {
+        Console.WriteLine($"{nombre} {apellido}");
+    }
+}
